c44f1ce8e58b7096ec22dd22a144add12b71129a
c0venn <fabichidori2@gmail.com>
2022-08-17 04:28:08 +0000

    home

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="" >
+        <h1>Rick app</h1>  
       </header>
+      <div>
+        <h1 className="text-center">¿Qué buscamos?</h1>
+        <div className="d-flex flex-column" >
+        <button><a href="/characters">Personajes</a></button>
+        <button><a href="/locations">Locaciones</a></button>
+        <button><a href="/episodes">Episodios</a></button>
+        </div>
+      </div>
     </div>
   );
 }
